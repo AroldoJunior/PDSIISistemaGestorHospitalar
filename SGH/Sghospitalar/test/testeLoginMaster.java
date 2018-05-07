e891e360b15caa7049926dde7ccdcc94e807a7ae
Aroldo Júnior <aroldojrall@gmail.com>
2018-05-07 08:47:42 -0300

    Reconstrução dos Testes da Função Login do Master

--- SGH/Sghospitalar/test/testeLoginMaster.java
+++ SGH/Sghospitalar/test/testeLoginMaster.java
@@ -1,8 +1,24 @@
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
 
 public class testeLoginMaster {
     
+    public testeLoginMaster() {
     
+    }
     
+    @Before
+    public void setUp() {
+        
+    }
     
+    @After
+    public void tearDown() {
+        
+    }
     
 }
