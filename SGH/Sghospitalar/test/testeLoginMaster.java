6cb09cc0af000f54997f9f3947dd6725482bbf7c
Aroldo Júnior <aroldojrall@gmail.com>
2018-05-07 10:06:41 -0300

    Inclusão de Testes do Login do Master

--- SGH/Sghospitalar/test/testeLoginMaster.java
+++ SGH/Sghospitalar/test/testeLoginMaster.java
@@ -1,3 +1,4 @@
+import View.ab_LoginMaster;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -7,17 +8,44 @@ import static org.junit.Assert.*;
 
 public class testeLoginMaster {
     
+    ab_LoginMaster l;
+    boolean respostaBranco = false;
+    
     public testeLoginMaster() {
     
     }
     
     @Before
     public void setUp() {
+        l = new ab_LoginMaster();
         
     }
     
     @After
     public void tearDown() {
+        l = null;
+        
+    }
+    
+    @Test
+    public void testeLoginEmBranco(){
+        // Dado que... 
+        
+        // Quando...
+        respostaBranco = l.loginEmBranco("","");
+        // Então...
+        assertEquals("Nao deveria estar em branco!", respostaBranco, true);
+        
+    }
+    
+    @Test
+    public void testeSenhaEmBranco(){
+           // Dado que... 
+        
+        // Quando...
+        respostaBranco = l.loginEmBranco("","");
+        // Então...
+        assertEquals("Nao deveria estar em branco!", respostaBranco, true);
         
     }
     
