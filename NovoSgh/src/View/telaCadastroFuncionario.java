d97b2348a4294b77efc723d8ab2e858dce850d90
“cabrutosr” <eduardo-silvarocha@hotmail.com>
2018-05-22 20:23:50 -0300

    “Adição_das_blindagens_cadastro”

--- NovoSgh/src/View/telaCadastroFuncionario.java
+++ NovoSgh/src/View/telaCadastroFuncionario.java
@@ -1,5 +1,7 @@
 package View;
 
+import Controller.insereFuncionario;
+import Model.Funcionario;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
@@ -549,7 +551,7 @@ public final class telaCadastroFuncionario implements ActionListener {
         panel.add(panelMenuSombra);
 
         frame = new JFrame();
-        frame.setTitle("Cadastro - Funcionário");
+        frame.setTitle("Cadastro - FuncionÃ¡rio");
         frame.setSize(900, 559);
         frame.setResizable(false);
         frame.setLocationRelativeTo(null);
@@ -558,11 +560,78 @@ public final class telaCadastroFuncionario implements ActionListener {
         frame.setContentPane(panel);
 
     }
+    
+    public boolean blingadegens(String nome, String cpf, String rg, String cargo,String senha, String email, String num){
+        
+        boolean resposta = false;
+        boolean aux1 = campoEmBranco(nome);
+        boolean aux2 = campoEmBranco(cpf);
+        boolean aux3 = campoEmBranco(rg);
+        boolean aux4 = campoEmBranco(cargo);
+        boolean aux5 = campoEmBranco(senha);
+        boolean aux6 = campoEmBranco(email);
+        boolean aux7 = campoEmBranco(num);
+        boolean aux8 = conterNumeros(nome);
+        boolean aux9 = conterNumeros(cargo);
+        boolean aux10 = conterNumeros(cpf);
+        boolean aux11 = conterNumeros(rg);
+        boolean aux12 = confirmarSenha(senha,senha);
+        boolean aux13 = cpfNumerosSize(cpf);
+        boolean aux14 = rgNumerosSize(rg);
+        boolean aux15 = contemLetras(cpf);  
+        boolean aux16 = contemLetras(rg);
+        boolean aux17 = campoSemEspaco(cpf);
+        boolean aux18 = campoSemEspaco(rg);
+        boolean aux19 = campoSemEspaco(senha);
+        boolean aux20 = campoSemEspaco(email);
+        boolean aux21 = campoNaoTemCaracteresEspeciais(nome);
+        boolean aux22 = campoNaoTemCaracteresEspeciais(cpf);
+        boolean aux23 = campoNaoTemCaracteresEspeciais(rg);
+        boolean aux24 = campoNaoTemCaracteresEspeciais(num);
+        boolean aux25 = emailEstrutura(email);
+
+        if (aux1 == false && aux2 == false && aux3 == false && aux4 == false && aux5 == false && aux6 == false && aux7 == false && aux8 == false && aux9 == false && aux10 == true && aux11 == true && aux12 == true && aux13 == true && aux14 == true && aux15 == false && aux16 == false && aux17 == false && aux18 == false && aux19 == false && aux20 == false && aux21 == true && aux22 == false && aux23 == false && aux24 == false && aux25 == true)
+            resposta = true;
+            
+        return resposta;
+        
+    }
 
     @Override
     public void actionPerformed(ActionEvent e) {
         
         if (e.getSource() == bCadastrar) {
+            
+            insereFuncionario insere = new insereFuncionario();
+
+                     String nom = textNome.getText();
+                    String cpf = textCpf.getText();
+                    String rg = textRg.getText();
+                    String cargo = textCargo.getText();
+                    String senh = textSenha.getText();
+                    String email = textEmail.getText();
+                    String tel = textTelefone.getText();    
+            
+            Funcionario funcionario = new Funcionario(
+                    nom,
+                    cpf,
+                    rg,
+                    cargo,
+                    senh,
+                    email,
+                    tel
+            );
+            if(blingadegens(nom,cpf,rg,cargo,senh,email,tel)==true){
+                insere.Cadastra(funcionario);
+                frame.dispose();
+            }else{
+                JOptionPane.showMessageDialog(frame ,"cadastro não realizado!\n Favor conferir os dados digitados!");
+            }
+                
+
+            
+            
+            
             
         }
         
@@ -570,6 +639,129 @@ public final class telaCadastroFuncionario implements ActionListener {
             frame.dispose();
         }
     }
+    
+     // Blindagens (Testes) //
+    
+    public boolean campoEmBranco(String login){
+       
+        boolean resposta = false;
+        
+        if(login.isEmpty())
+            resposta = true;
+       
+        return resposta;
+        
+    }
+    
+    public boolean confirmarSenha(String senha, String confSenha){
+        boolean valida = false;
+        
+        if(confSenha.equals(senha)){
+            valida = true;
+        }
+        return valida;
+    }
+    
+    public boolean senhaEmBranco(String senha){
+       
+        boolean resposta = false;
+        
+        if(senha.isEmpty())
+            resposta = true;
+       
+        return resposta;
+        
+    }
+    
+    public boolean emailEstrutura(String texto){
+        if (texto.contains("@")&&texto.contains(".com")){
+	    return true;
+	}else{
+	    return false;
+	}
+    }
+    
+    public boolean campoNaoTemCaracteresEspeciais(String login){
+	
+        boolean resposta = false;
+        
+        if (login.matches("[a-zA-Z_][a-zA-Z0-9_]*"))
+            return true;
+        
+        return resposta;
+        
+    }
+    
+    
+    public boolean conterNumeros(String texto){
+        for (int i = 0; i < texto.length(); i++) {
+            if (Character.isDigit(texto.charAt(i))==true){
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    public Boolean cpfNumerosSize(String cpf){
+        if(cpf.length()==11){
+            return true;
+        }else{
+            return false;
+        }
+    }
+    
+    public Boolean rgNumerosSize(String rg){
+        if(rg.length()==7){
+            return true;
+        }else{
+            return false;
+        }
+    }
+    
+    public boolean contemLetras( String s ) {  
+        for ( int i = 0; i < s.length(); i++ ) { 
+	    if ( Character.isDigit( s.charAt(i) )==true) {  
+		return false;
+	     }  
+	}  
+	return true;  
+   }
+    
+    public boolean campoSemEspaco(String login){
+        
+        boolean resposta = false;
+        
+        if (login.contains(" "))
+	    resposta = true;
+
+        return resposta;
+            
+    }
+    
+    public boolean loginTamanho(String login){
+        
+        boolean valida = false;
+        
+        if (login.length() >= 6 && login.length() <=8 )
+            valida = true;
+        
+        return valida;
+        
+    }
+    
+    public boolean senhaTamanho(String senha){
+        
+        boolean valida = false;
+        
+        if (senha.length() >= 6 && senha.length() <=8 )
+            valida = true;
+        
+        return valida;
+        
+    }
+    
+    
+    
 
     public static void main(String args[]) {
 
