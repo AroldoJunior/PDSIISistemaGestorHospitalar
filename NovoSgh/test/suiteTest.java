e0162770b62ddd208d2a46c89f21528f5db6701a
“cabrutosr” <eduardo-silvarocha@hotmail.com>
2018-06-12 23:08:37 -0300

    “Classe_Suite_Teste”

---
+++ NovoSgh/test/suiteTest.java
@@ -0,0 +1,18 @@
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+/**
+ *
+ * @author CABRUTO
+ */
+ @RunWith(Suite.class)
+
+    @Suite.SuiteClasses({
+            Cdastra.class,
+            testeLoginMaster.class
+})
+
+public class suiteTest {
+    
+   
+}
